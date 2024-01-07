a768437129203b40f395b77da9d50fb9e5e816b1
Ernestolop <ernestodaniellopez504@gmail.com>
2024-01-06 22:41:27 -0300

    feat: Add Toggle Menu Mobile

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,7 +1,17 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
+import { useState } from 'react';
 
 export const NavBar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
         <div className='navbar mw-950'>
             <Link href="/" className='navbar__logo'>
@@ -13,13 +23,24 @@ export const NavBar = () => {
                 />
             </Link>
             <nav className='nav'>
-                
-                <button type='button'>
-                    <svg className='nav__hamburger' width={30} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                        <path fillRule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
-                    </svg>
+                <button onClick={toggleMenu} type='button'>
+                    {
+                        menuOpen ?
+                            (
+                                <svg className='nav__hamburger' width={30} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                                    <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                                </svg>
+                            )
+                            :
+                            (
+                                <svg className='nav__hamburger' width={30} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                                    <path fillRule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+                                </svg>
+                            )
+
+                    }
                 </button>
-                <ul className='nav__ul nav__ul--md'>
+                <ul className={`nav__ul ${menuOpen ? '' : 'nav__ul--mdhide'}`}>
                     <li>
                         <Link href="/" className='nav__link'>Inicio</Link>
                     </li>
