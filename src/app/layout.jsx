16a54351a5ac404f8c3de7e773a6fc969d814be2
Ernestolop <ernestodaniellopez504@gmail.com>
2024-01-14 10:35:15 -0300

    feat: change font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,14 @@
-import { Inter } from 'next/font/google'
+import { Dosis } from 'next/font/google'
 import './globals.css'
 import './index.css'
 import { NavBar, Footer } from '@/components'
 
-const inter = Inter({ subsets: ['latin'] })
+const dosis = Dosis({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} body`}>
+      <body className={`${dosis.className} body`}>
         <header className='header'>
           <NavBar />
         </header>
