41c022c668c5e530ae8d70e527c6fa6d66ebafee
Ernestolop <ernestodaniellopez504@gmail.com>
2024-02-15 08:17:07 -0300

    fix: update seed coordenates data

--- src/seed/seed.js
+++ src/seed/seed.js
@@ -28,95 +28,95 @@ export const branches = [
         address: 'Avda. Eusebio Ayala 1947.',
         departament: 'Central.',
         tel: '(021) 202 913 / 15',
-        latitude: -55.57824311,
-        longitude: -27.03531182
+        latitude: -27.03531182,
+        longitude: -55.57824311
     }
     , {
         city: 'Campo 9',
         address: 'Avda. Eusebio Ayala 1947.',
         departament: 'Central.',
         tel: '(021) 202 913 / 15',
-        latitude: -55.71566881,
-        longitude: -25.37788224
+        latitude: -25.37788224,
+        longitude: -55.71566881
     }
     , {
         city: 'Casa Central Tracto',
         address: 'Avda. Eusebio Ayala 1947.',
         departament: 'Central.',
         tel: '(021) 202 913 / 15',
-        latitude: -57.60049225,
-        longitude: -25.32265071
+        latitude: -25.32265071,
+        longitude: -57.60049225
     }
     , {
         city: 'Filadelfia',
         address: 'Avda. Eusebio Ayala 1947.',
         departament: 'Central.',
         tel: '(021) 202 913 / 15',
-        latitude: -60.03389414,
-        longitude: -22.36392617
+        latitude: -22.36392617,
+        longitude: -60.03389414
     }
     , {
         city: 'Katuete',
         address: 'Avda. Eusebio Ayala 1947.',
         departament: 'Central.',
         tel: '(021) 202 913 / 15',
-        latitude: -54.78588365,
-        longitude: -24.25773186
+        latitude: -24.25773186,
+        longitude: -54.78588365
     }
     , {
         city: 'Loma Plata',
         address: 'Avda. Eusebio Ayala 1947.',
         departament: 'Central.',
         tel: '(021) 202 913 / 15',
-        latitude: -59.83671933,
-        longitude: -22.40017167
+        latitude: -22.40017167,
+        longitude: -59.83671933
     }
     , {
         city: 'Mariano Roque Alonso',
         address: 'Avda. Eusebio Ayala 1947.',
         departament: 'Central.',
         tel: '(021) 202 913 / 15',
-        latitude: -57.53053957,
-        longitude: -25.21731966
+        latitude: -25.21731966,
+        longitude: -57.53053957
     }
     , {
         city: 'San Alberto',
         address: 'Avda. Eusebio Ayala 1947.',
         departament: 'Central.',
         tel: '(021) 202 913 / 15',
-        latitude: -54.94560888,
-        longitude: -24.93625363
+        latitude: -24.93625363,
+        longitude: -54.94560888
     },
     {
         city: 'San Juan Bautista',
         address: 'Avda. Eusebio Ayala 1947.',
         departament: 'Central.',
         tel: '(021) 202 913 / 15',
-        latitude: -57.13287592,
-        longitude: -26.66591426
+        latitude: -26.66591426,
+        longitude: -57.13287592
     },
     {
         city: 'Santa Rita',
         address: 'Avda. Eusebio Ayala 1947.',
         departament: 'Central.',
         tel: '(021) 202 913 / 15',
-        latitude: -55.07646973,
-        longitude: -25.77804069
+        latitude: -25.77804069,
+        longitude: -55.07646973
     },
     {
         city: 'Santa Rosa',
         address: 'Avda. Eusebio Ayala 1947.',
         departament: 'Central.',
         tel: '(021) 202 913 / 15',
-        latitude: -56.43165158,
-        longitude: -23.80221277
+        latitude: -23.80221277,
+        longitude: -56.43165158
     },
     {
         city: 'Taller MRA',
         address: 'Avda. Eusebio Ayala 1947.',
         departament: 'Central.',
         tel: '(021) 202 913 / 15',
-        latitude: -57.5229839,
-        longitude: -25.21561195
+        latitude: -25.21561195,
+        longitude: -57.5229839,
     }
 ]
