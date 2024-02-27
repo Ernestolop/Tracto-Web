ceb799fa2bfa7609d604868e28c1fcff6c277d55
Ernesto Lopez <daniel.lopez@cast.com.py>
2024-02-27 11:41:59 -0300

    feat: update seed data for branches

--- src/seed/branches.js
+++ src/seed/branches.js
@@ -1,98 +1,88 @@
 export const branches = [
     {
-        city: 'Bella Vista',
-        address: 'Avda. Eusebio Ayala 1947.',
-        departament: 'Central.',
-        tel: '(021) 202 913 / 15',
-        latitude: -27.03531182,
-        longitude: -55.57824311
-    }
-    , {
-        city: 'Campo 9',
-        address: 'Avda. Eusebio Ayala 1947.',
-        departament: 'Central.',
-        tel: '(021) 202 913 / 15',
-        latitude: -25.37788224,
-        longitude: -55.71566881
-    }
-    , {
         city: 'Casa Central Tracto',
-        address: 'Avda. Eusebio Ayala 1947.',
-        departament: 'Central.',
-        tel: '(021) 202 913 / 15',
+        address: 'Dr. Teodisio González y Tte B. González.',
+        departament: 'Asunción.',
+        tel: '555 613 / 4',
         latitude: -25.32265071,
         longitude: -57.60049225
     }
     , {
+        city: 'Bella Vista Sur',
+        address: 'Ruta 6 KM 49.',
+        departament: 'Itapúa.',
+        tel: '0767 240 562',
+        latitude: -27.03531182,
+        longitude: -55.57824311
+    }
+    , {
         city: 'Filadelfia',
-        address: 'Avda. Eusebio Ayala 1947.',
-        departament: 'Central.',
-        tel: '(021) 202 913 / 15',
+        address: 'Centro de Filadelfia.',
+        departament: 'Boqueron.',
+        tel: '0491 432 450',
         latitude: -22.36392617,
         longitude: -60.03389414
     }
     , {
         city: 'Katuete',
-        address: 'Avda. Eusebio Ayala 1947.',
-        departament: 'Central.',
-        tel: '(021) 202 913 / 15',
+        address: 'Avda. de las Residentas, centro.',
+        departament: 'Canindeyú.',
+        tel: '0471 234 273',
         latitude: -24.25773186,
         longitude: -54.78588365
     }
     , {
         city: 'Loma Plata',
-        address: 'Avda. Eusebio Ayala 1947.',
-        departament: 'Central.',
-        tel: '(021) 202 913 / 15',
+        address: 'Avda. Centra c/ La Rotonda.',
+        departament: 'Boquerón.',
+        tel: '0492 252 35',
         latitude: -22.40017167,
         longitude: -59.83671933
     }
     , {
-        city: 'Mariano Roque Alonso',
-        address: 'Avda. Eusebio Ayala 1947.',
+        city: 'Taller MRA',
+        address: 'Mrcal. López c/ Capitán Cabral.',
         departament: 'Central.',
-        tel: '(021) 202 913 / 15',
+        tel: '021 751 365',
         latitude: -25.21731966,
         longitude: -57.53053957
     }
     , {
         city: 'San Alberto',
-        address: 'Avda. Eusebio Ayala 1947.',
-        departament: 'Central.',
-        tel: '(021) 202 913 / 15',
+        address: 'Super Carretera Itaipú.',
+        departament: 'Alto Paraná.',
+        tel: '067 720 361',
         latitude: -24.93625363,
         longitude: -54.94560888
     },
     {
-        city: 'San Juan Bautista',
-        address: 'Avda. Eusebio Ayala 1947.',
-        departament: 'Central.',
-        tel: '(021) 202 913 / 15',
-        latitude: -26.66591426,
-        longitude: -57.13287592
-    },
-    {
         city: 'Santa Rita',
-        address: 'Avda. Eusebio Ayala 1947.',
-        departament: 'Central.',
-        tel: '(021) 202 913 / 15',
+        address: 'Ruta 6 KM 213,5.',
+        departament: 'Alto Paraná.',
+        tel: '0673 221 675',
         latitude: -25.77804069,
         longitude: -55.07646973
     },
     {
         city: 'Santa Rosa',
-        address: 'Avda. Eusebio Ayala 1947.',
-        departament: 'Central.',
-        tel: '(021) 202 913 / 15',
+        address: 'Ruta 3 General Aquino.',
+        departament: 'San Pedro.',
+        tel: '0433 240 144',
         latitude: -23.80221277,
         longitude: -56.43165158
     },
     {
-        city: 'Taller MRA',
-        address: 'Avda. Eusebio Ayala 1947.',
+        city: 'Mariano Roque Alonso',
+        address: 'Ruta Transchaco KM 15.',
         departament: 'Central.',
-        tel: '(021) 202 913 / 15',
+        tel: '021 752 997',
         latitude: -25.21561195,
         longitude: -57.5229839,
+    }, {
+        city: 'Campo 9',
+        address: 'Caaguazú.',
+        departament: 'Central.',
+        latitude: -25.37788224,
+        longitude: -55.71566881
     }
 ]
